b4b8edfb70e1a524c9e812b93069c037725a11c9
Johannes-Forsting <johannes.forsting@gmail.com>
2022-06-27 12:14:32 +0200

    commit 4

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -9,78 +9,133 @@ import org.springframework.web.context.request.WebRequest;
 @Controller
 public class IndexController {
 
+    String svar1 = "seks";
+    String svar1Buffer = "6";
+
+    String svar2 = "gudhjem";
+    String svar2Buffer = "gudhjem havn";
+
+    String svar3 = "6009";
+    String svar3Buffer = "år 6009";
+
+    String svar4 = "test";
+    String svar4Buffer = "tester";
+
 
     @RequestMapping("/")
     public String frontpage(){
         return "index";
     }
 
-    @GetMapping("/page1")
-    public String page1(){
+    @GetMapping("/post1")
+    public String post1(){
 
-        return "page1";
+        return "post1";
     }
 
-    @GetMapping("/page2")
-    public String page2(){
+    @RequestMapping("/post1svar")
+    public String post1svar(WebRequest dataFromForm){
+        String answer = dataFromForm.getParameter("answer");
+        if (answer != null) {
 
-        return "page2";
+            if (answer.equalsIgnoreCase(svar1) || answer.equals(svar1Buffer)) {
+
+                return "post1kort";
+            }
+        }
+        return "index";
     }
 
-    @GetMapping("/page3")
-    public String page3(){
+    @GetMapping("/post2")
+    public String post2(){
 
-        return "page3";
+        return "post2";
     }
 
-    @GetMapping("/page4")
-    public String page4(){
+    @RequestMapping("/post2svar")
+    public String post2svar(WebRequest dataFromForm){
+        String answer = dataFromForm.getParameter("answer");
+        if (answer != null) {
 
-        return "page4";
+            if (answer.equalsIgnoreCase(svar2) || answer.equalsIgnoreCase(svar2Buffer)) {
+
+                return "post2kort";
+            }
+        }
+        return "index";
     }
 
-    @GetMapping("/page5")
-    public String page5(){
+    @GetMapping("/post3")
+    public String post3(){
 
-        return "page5";
+        return "post3";
     }
 
-    @GetMapping("/page6")
-    public String page6(){
+    @GetMapping("/post3svar")
+    public String post3svar(WebRequest dataFromForm){
+        String answer = dataFromForm.getParameter("answer");
+        if (answer != null) {
+            if (answer.equalsIgnoreCase(svar3) || answer.equalsIgnoreCase(svar3Buffer)) {
 
-        return "page6";
+                return "post3kort";
+            }
+        }
+        return "index";
     }
 
-    @GetMapping("/page7")
-    public String page7(){
+    @GetMapping("/post4")
+    public String post4(){
 
-        return "page7";
+        return "post4";
     }
 
-    @GetMapping("/page8")
-    public String page8(){
+    @GetMapping("/post4svar")
+    public String post4svar(WebRequest dataFromForm){
+        String answer = dataFromForm.getParameter("answer");
+        if (answer != null) {
+            if (answer.equalsIgnoreCase(svar4) || answer.equalsIgnoreCase(svar4Buffer)) {
 
-        return "page8";
+                return "post3kort";
+            }
+        }
+        return "index";
     }
 
-    @GetMapping("/page9")
-    public String page9(){
+    @GetMapping("/post5")
+    public String post5(){
 
-        return "page9";
+        return "post5";
     }
 
-    @GetMapping("/page10")
-    public String page10(){
+    @GetMapping("/post5svar")
+    public String post5svar(){
 
-        return "page10";
+        return "index";
     }
 
     @PostMapping("/test")
     public String test(WebRequest dataFromForm){
-        int number = Integer.parseInt(dataFromForm.getParameter("number"));
-        if(number==345){
-            return "page10";
+        String answer1 = dataFromForm.getParameter("post1");
+        String answer2 = dataFromForm.getParameter("post2");
+        String answer3 = dataFromForm.getParameter("post3");
+        String answer4 = dataFromForm.getParameter("post4");
+
+        if(answer1 != null && answer2 != null && answer3 != null && answer4 != null){
+            if (!(answer1.equalsIgnoreCase(svar1) || answer1.equalsIgnoreCase(svar1Buffer))) {
+                return "index";
+            }
+            if (!(answer2.equalsIgnoreCase(svar2) || answer2.equalsIgnoreCase(svar2Buffer))) {
+                return "index";
+            }
+            if (!(answer3.equalsIgnoreCase(svar3) || answer3.equalsIgnoreCase(svar3Buffer))) {
+                return "index";
+            }
+            if (!(answer4.equalsIgnoreCase(svar4) || answer4.equalsIgnoreCase(svar4Buffer))) {
+                return "index";
+            }
+            return "post5kort";
         }
+
         return "index";
     }
 
